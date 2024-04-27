eb7fee78621276536f3ae91b6791c3e94ab99f31
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-04-27 19:26:04 +0530

    datatype2

---
+++ datatype2.js
@@ -0,0 +1,19 @@
+let score =  "22aa";
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+console.log(valueInNumber)
+
+
+// "33" == 33
+// "22aa" == nan
+
+// true = 1 ; flase = 0
+// "" = false 
+// "krishna" = true
+
