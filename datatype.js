2c39b91a8dd9e46b198bc1861b1c5d15f839f5e0
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-04-27 09:46:42 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,34 @@
+"use strict"; // all js code are use in new version
+
+// alert(3 + 3) using in not in nodejs but it use in consol
+
+
+let age = 60 ;//number = 2 
+
+let name = "krishna" //string = "krishna"
+
+let state; //undefined 
+
+let krinna  = true; // boolean = true
+
+
+/* all data type -- premitive datatype  
+
+number = 2 to  2*power 53
+
+bigInt 
+
+string = "krishna" "symbol"
+
+boolean = true & false
+
+null  = standalone value 
+
+undefined 
+
+symbol = unique 
+*/
+
+
+// object 
+console.log(typeof String)
