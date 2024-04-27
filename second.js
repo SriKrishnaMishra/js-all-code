6597187cec0022840160c8556f1dc692a7bcd60d
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-04-27 09:21:51 +0000

    new notes

---
+++ second.js
@@ -0,0 +1,24 @@
+const name = "krishna";
+let email = "krishna@gmail.com"
+var accountPassword = "1321"
+accounttown = "roorkee"
+let accountstate;
+
+// name = "krihna mishra"
+
+email = "krishnamishra@gmail.com"
+accountPassword = "09242"
+accounttown = "jamtara"
+
+// console.log(name);
+// console.log(email);
+// console.log(accountPassword);
+// console.log(accounttown);
+
+/*
+Not use var
+Because of issue in block scope and functional scope
+
+*/
+
+console.table([name, email, accountPassword, accounttown, accountstate]);
