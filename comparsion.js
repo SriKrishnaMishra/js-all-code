cdf1710a22d79a7c1bf545303e4416f3ced83529
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-04-28 09:42:43 +0000

    comparsion

---
+++ comparsion.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 2);
+// console.log(2 != 1);
+
+{
+console.log("2" > 1); // please make sure both datatype is some in very time
+console.log("02" > 1);
+}
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true why commit it
+
+
+// === good for check
+
+console.log(3 === "2"); // false
