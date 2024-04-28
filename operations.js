7a59055973eff7147573c0dbb9dc97cfa7fb32e7
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-04-28 09:37:34 +0000

    operations

---
+++ operations.js
@@ -0,0 +1,29 @@
+let value = 3;
+
+let negValue = -value; // beacuse of -negative value assign
+console.log(negValue);
+
+
+console.log(2 + 3);
+console.log(3 - 2);
+console.log(6 * 3);
+console.log(4 ** 2);
+console.log(4 / 2);
+console.log(3 % 2); 
+
+
+let a1 = "krishna ";
+let a2 = "Mishra ";
+let a3 = a1 + a2;
+console.log(a3);
+
+console.log("2" + 2);
+console.log(2 + "2");
+console.log("1" + 2 + 2); // change value
+console.log(1 + 2 + "2");  // change value
+
+console.log(+true);
+console.log(+"");
+
+// prefix js mdn reading
+// postfix js mdn reading
