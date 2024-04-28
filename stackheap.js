402f092c49dc149a2a2363fba348e958d36c3acf
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-04-28 11:23:23 +0000

    stackheap

---
+++ stackheap.js
@@ -0,0 +1,30 @@
+
+// stack (primitive data type) // copy value
+// heap (non primitive data type) // refernce value
+
+
+// stack (primitive datatype)
+let myname = "sri krishan mishra"
+
+let anothername  = myname
+anothername = "krishna mishra"
+
+console.log(myname);
+console.log(anothername);
+
+
+
+// heap and non premitive datatype
+
+let userOne = {
+
+    name:"krishna",
+    roll: 21,
+}
+
+let userTwo = userOne
+userTwo.name = "hanuman" 
+
+
+console.log(userOne.name);
+console.log(userTwo.name);
