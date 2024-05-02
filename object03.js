b93e5b7ddce47c18545fa5a55e9ede34ed0fdb92
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-05-02 07:12:55 +0000

    object03

---
+++ object03.js
@@ -0,0 +1,14 @@
+const cousre = {
+
+    couseName : "js best playlist",
+    price : "999",
+    cousreBest : "js"
+}
+
+ const {cousreBest} = cousre  // distructure
+
+const {cousreBest : best} = cousre  // both are same but divided into small part
+
+console.log(cousreBest);
+console.log(best);
+
