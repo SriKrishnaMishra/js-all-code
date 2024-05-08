3323af6f4f436647640d7d3b357be836f5e86405
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-05-08 05:54:27 +0000

    scope1

---
+++ scope1.js
@@ -0,0 +1,23 @@
+// let a = 10;
+// const b = 20;
+// var c = 50;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+ let a = 200;
+if (true) {
+
+    let a = 10
+    const b = 20
+    var c = 50
+    
+    // block scope
+    console.log("mango: ", a);
+}
+
+
+console.log(a);
+// console.log(b);
+console.log(c);
