c47f5ccc366287891d501bf0114635bfbc290b1d
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-05-08 07:15:11 +0000

    iife

---
+++ iife.js
@@ -0,0 +1,16 @@
+
+(function pen() {
+
+    console.log(`db is connected`);
+})  ();
+
+(function penorcopy() {
+
+    console.log(`db is connected two`);
+})  ();
+
+
+( (cousrename) => {
+
+    console.log(`db is connected ${cousrename}`);
+} )  ("javascript");
