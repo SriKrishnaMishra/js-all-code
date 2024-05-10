4ef6ed2044d604ca8eea311cab31e2c2eb4bc4e6
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-05-10 05:03:16 +0000

    if_statement

---
+++ if_statement.js
@@ -0,0 +1,42 @@
+if(2 === 2) {
+ 
+    console.log("true");
+}
+else {
+
+    console.log("false");
+}
+
+//  this are the symbol 
+// < , > , <= , >= , == , != , ===, !==
+
+const tempeture = 40 
+
+if(tempeture > 40) {
+
+   console.log("tempeture is 32");
+
+}
+else {
+
+    console.log("tempeture is 42");
+}
+
+////////////////
+
+const score = 39
+
+if(score > 33 ) {
+
+    var num = "fly"
+    console.log(`user num : ${num}`);
+}
+
+
+//////////////
+
+
+const balance = 2100
+
+//if(balance > 2000) console.log("test1"), console.log("test2");
+
