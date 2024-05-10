113477c43adbf73edb8d60710e0f779098487977
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-05-10 07:01:55 +0000

    switch_statement

---
+++ switch_statement.js
@@ -0,0 +1,58 @@
+const month = "april";
+
+switch (month) {
+    case "jan":
+        
+    console.log("jan");
+        break;
+
+    case "fed":
+        
+    console.log("feb");
+        break;
+    case "march":
+        
+    console.log("march");
+        break;
+
+    case "april":
+        
+    console.log("april");
+        break;
+
+    default:
+
+    console.log("not month");
+        break;
+}
+
+
+//////// same but different name 
+
+const monthly = 3;
+
+switch (monthly) {
+    case 1:
+        
+    console.log("jan");
+        break;
+
+    case 2:
+        
+    console.log("feb");
+        break;
+    case 3:
+        
+    console.log("march");
+        break;
+
+    case 4:
+        
+    console.log("april");
+        break;
+
+    default:
+
+    console.log("not month");
+        break;
+}
