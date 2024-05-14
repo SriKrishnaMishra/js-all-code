bfbe9778900ebca78dc60df701adce6324264ef7
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-05-14 06:38:58 +0000

    while_loop

---
+++ while_loop.js
@@ -0,0 +1,20 @@
+let index = 0;
+
+ while( index <= 10) {
+
+    console.log(`value of index ${index}`);
+    index = index + 2;
+
+ }
+
+ let myArray = ['krishna', 'shiv', 'hanuman','ram']
+
+ console.log(myArray.length);
+
+ let arr = 0;
+
+ while(arr < myArray.length) {
+
+    console.log(`vlaue is ${myArray[arr]}`);
+    arr = arr + 1;
+ }
