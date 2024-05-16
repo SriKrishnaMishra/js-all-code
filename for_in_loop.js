726a3cc27a0b9401f023b0c3b1276d3d87a5aaf0
Sri Krishna Mishra <131592380+SriKrishnaMishra@users.noreply.github.com>
2024-05-16 04:19:03 +0000

    for_in_loop

---
+++ for_in_loop.js
@@ -0,0 +1,27 @@
+ ///// print obj         ///////
+const myObj = {
+
+    js: 'javascript',
+    cp:' c++',
+    py:' python',
+
+}
+
+for (const key in myObj) {
+    
+    console.log(`${key} name is ${myObj[key]}`);
+
+}
+
+///////////   in array    ////////
+
+const name = ["krisha", "shiv", "ram", "hanuman"]
+
+
+for (const key in name) {
+   
+    console.log(name[key]);
+}
+
+
+///////
